ecc03f68bbab1ba6ab008115f1cb2cbc51361b64
André Lucas <andre@andrelug.com>
2016-11-02 17:41:15 -0500

    facebook

--- config/auth.js
+++ config/auth.js
@@ -5,45 +5,45 @@ var express = require('express'),
 if('development' == app.get('env')) {
     module.exports = {
 
-	    'facebookAuth' : {
-		    'clientID' 		: '1519238171626381', // your App ID
-		    'clientSecret' 	: '5c99dddcfef93210d4b2101c5c196d07', // your App Secret
-		    'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
-	    },
+        'facebookAuth' : {
+            'clientID' 		: '1662382147405788', // your App ID
+            'clientSecret' 	: '67dc52d5d0c9a6e0b2afbd3c7318277b', // your App Secret
+            'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
+        },
 
-	    'twitterAuth' : {
-		    'consumerKey' 		: 'd7Nm9kGmddTbp8bVPd3IRWpAq',
-		    'consumerSecret' 	: 'x2FJbmrix0HaHd0HXWpw5mLVbRvmalCnWLc83bxv481KMd9QeL',
-		    'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
-	    },
+        'twitterAuth' : {
+            'consumerKey' 		: 'd7Nm9kGmddTbp8bVPd3IRWpAq',
+            'consumerSecret' 	: 'x2FJbmrix0HaHd0HXWpw5mLVbRvmalCnWLc83bxv481KMd9QeL',
+            'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
+        },
 
-	    'googleAuth' : {
-		    'clientID' 		: '255367409904.apps.googleusercontent.com',
-		    'clientSecret' 	: '99dda7gFKNaONJL6rg23odNZ',
-		    'callbackURL' 	: 'http://localhost:3000/auth/google/callback'
-	    }
+        'googleAuth' : {
+            'clientID' 		: '255367409904.apps.googleusercontent.com',
+            'clientSecret' 	: '99dda7gFKNaONJL6rg23odNZ',
+            'callbackURL' 	: 'http://localhost:3000/auth/google/callback'
+        }
 
     };
 }else{
     module.exports = {
 
-	    'facebookAuth' : {
-		    'clientID' 		: '253873004991221', // your App ID
-		    'clientSecret' 	: 'c7fe9bd5f0188bb89e104f82417ae34a', // your App Secret
-		    'callbackURL' 	: 'http://igluonline.com/auth/facebook/callback'
-	    },
+        'facebookAuth' : {
+            'clientID' 		: '253873004991221', // your App ID
+            'clientSecret' 	: 'c7fe9bd5f0188bb89e104f82417ae34a', // your App Secret
+            'callbackURL' 	: 'http://nossoamigo.org/auth/facebook/callback'
+        },
 
-	    'twitterAuth' : {
-		    'consumerKey' 		: 'd7Nm9kGmddTbp8bVPd3IRWpAq',
-		    'consumerSecret' 	: 'x2FJbmrix0HaHd0HXWpw5mLVbRvmalCnWLc83bxv481KMd9QeL',
-		    'callbackURL' 		: 'http://www.gueime.com.br/auth/twitter/callback'
-	    },
+        'twitterAuth' : {
+            'consumerKey' 		: 'd7Nm9kGmddTbp8bVPd3IRWpAq',
+            'consumerSecret' 	: 'x2FJbmrix0HaHd0HXWpw5mLVbRvmalCnWLc83bxv481KMd9QeL',
+            'callbackURL' 		: 'http://www.gueime.com.br/auth/twitter/callback'
+        },
 
-	    'googleAuth' : {
-		    'clientID' 		: '255367409904.apps.googleusercontent.com',
-		    'clientSecret' 	: '99dda7gFKNaONJL6rg23odNZ',
-		    'callbackURL' 	: 'http://www.gueime.com.br/auth/google/callback'
-	    }
+        'googleAuth' : {
+            'clientID' 		: '255367409904.apps.googleusercontent.com',
+            'clientSecret' 	: '99dda7gFKNaONJL6rg23odNZ',
+            'callbackURL' 	: 'http://www.gueime.com.br/auth/google/callback'
+        }
 
     };
 }
